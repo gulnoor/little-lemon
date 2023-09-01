92da6b98155d45b15909bd890840e5f4ccbcc371
gulnoor <gulnoor5233@gmail.com>
2023-09-01 17:44:58 +0500

    added navigation rail layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NavigationRail from './Components/NavigationRail';
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      Home
+    <div className="App" >
+      <NavigationRail />
     </div>
   );
 }
---
+++ src/Components/NavigationRail.js
@@ -0,0 +1,25 @@
+import "./NavigationRail.css"
+const NavigationRail = () => {
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Menu</a>
+          </li>
+          <li>
+            <a href="">Reservation</a>
+          </li>
+          <li>
+            <a href="">Toggle Theme</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+export default NavigationRail;
