ea1936d71f71222d76d77a541f2efc0f1ce169b0
gulnoor <gulnoor5233@gmail.com>
2023-09-02 00:20:46 +0500

    added google material symbols in navigation rail

--- src/Components/NavigationRail.js
+++ src/Components/NavigationRail.js
@@ -1,24 +1,27 @@
 import { useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
-import "./NavigationRail.css"
-const NavigationRail = ({toggle}) => {
-  const colors = useContext(ThemeContext)
-
+import "./NavigationRail.css";
+const NavigationRail = ({ toggle }) => {
+  const colors = useContext(ThemeContext);
 
   return (
     <>
-      <nav style={{background:colors.secondary}}>
+      <nav style={{ background: colors.secondary }}>
         <ul>
           <li>
+            <span class="material-symbols-outlined">home</span>
             <a href="">Home</a>
           </li>
           <li>
+            <span class="material-symbols-outlined">restaurant_menu</span>
             <a href="">Menu</a>
           </li>
           <li>
+            <span class="material-symbols-outlined">table_restaurant</span>
             <a href="">Reservation</a>
           </li>
           <li>
+            <span class="material-symbols-outlined">dark_mode</span>
             <button onClick={toggle}>Toggle Theme</button>
           </li>
         </ul>
