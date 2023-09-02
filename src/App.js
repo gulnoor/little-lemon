b6473e2edce11685339f3fe608b95296dc5fba53
gulnoor <gulnoor5233@gmail.com>
2023-09-02 20:45:09 +0500

    added hero text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import Hero from "./Components/Hero";
 import "./css/tokens.css";
-import {  useRef } from "react";
+import Hero from "./Components/Hero";
 import NavigationRail from "./Components/NavigationRail";
-function App() {
-  const theme = useRef("light");
+import {  useRef } from "react";
 
+function App() {
+
+  const theme = useRef("light");
   const toggleTheme = () => {
     if (theme.current === "light") {
       document.querySelector("html").className = "dark";
@@ -17,7 +18,7 @@ function App() {
   };
 
   return (
-    <div className="App" style={{ height: "100vh", display: "flex" }}>
+    <div className="App" style={{ height: "100vh", display: "flex",background:"var(--md-sys-color-surface)" }}>
       <NavigationRail theme={theme.current} toggle={toggleTheme} />
       <div
         className="Content-container"
@@ -25,7 +26,6 @@ function App() {
           height: "100vh",
           overflowY: "scroll",
           width: "100%"
-         
         }}
       >
         <Hero />
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,13 +1,15 @@
-import "./Hero.css"
-import logo from "../assets/Asset 9@4x.png"
-const Hero = ()=>{
-    return(
-        <div className="Hero">
-            <div className="hero-text">
-                <h1 className="display-large">Little Lemmon</h1>
-            </div>
-                <img src={logo} alt="" />
-        </div>
-    )
-}
+import "./Hero.css";
+import logo from "../assets/Asset 9@4x.png";
+const Hero = () => {
+  return (
+    <div className="Hero">
+      <div className="hero-text">
+        <h1>Little Lemmon</h1>
+        <h2>Chicago</h2>
+        <span>traditional recipes served with a modern twist</span>
+      </div>
+      <img src={logo} alt="" />
+    </div>
+  );
+};
 export default Hero;
