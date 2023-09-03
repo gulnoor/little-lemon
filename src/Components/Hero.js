4570141948f872641e38613de7ffc81897269a6a
gulnoor <gulnoor5233@gmail.com>
2023-09-03 18:17:53 +0500

    commented out old code that isn't used anymore

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,25 +1,24 @@
 import "./Hero.css";
 import logo from "../assets/images/Asset 9@4x.png";
 import Button from "./Button/Button";
-import { createRef, useEffect, useRef, useState } from "react";
 const Hero = () => {
-  const heroRef = useRef(null);
-  let heroHeight = "";
-  const [logobgStyle,setlbgs] =useState({
-    background: "var(--md-sys-color-primary)",
-    borderRadius: "16px",
-    height:heroHeight
-  })
-  useEffect(()=>{
-    setlbgs(
-      logobgStyle=> {
-        return {...logobgStyle,height:window.getComputedStyle(heroRef.current).getPropertyValue("height")}
-      }
-    )
-  },[])
+  // const heroRef = useRef(null);
+  // let heroHeight = "";
+  // const [logobgStyle,setlbgs] =useState({
+  //   background: "var(--md-sys-color-primary)",
+  //   borderRadius: "16px",
+  //   height:heroHeight
+  // })
+  // useEffect(()=>{
+  //   setlbgs(
+  //     logobgStyle=> {
+  //       return {...logobgStyle,height:window.getComputedStyle(heroRef.current).getPropertyValue("height")}
+  //     }
+  //   )
+  // },[])
   // const heroHeight = window.getComputedStyle(heroRef.current).height;
   return (
-    <div ref={heroRef} className="Hero">
+    <div /* ref={heroRef}  */className="Hero">
       <div className="hero-text">
         <h1>Little Lemmon</h1>
         <h2>Chicago</h2>
