053f8c6e9e473df57e3c254d2a266989d157b8ad
gulnoor <gulnoor5233@gmail.com>
2023-09-17 18:21:45 +0500

    (fix: theme resets on page refresh) used localStorage API to store active theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import '@material/web/textfield/outlined-text-field'
 
 import "./App.css";
-import Hero from "./Components/Hero";
+
 import NavigationRail from "./Components/NavigationRail";
-import { useRef } from "react";
-import TopNav from "./Components/TopNav";
-import AboutUs from "./Components/AboutUs";
+import { useEffect, useRef, useState } from "react";
+
 import BookingPage from './Components/BookingPage';
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
@@ -13,20 +12,58 @@ import SubmissionDialogue from './Components/SubmissionDialogue';
 
 function App() {
 
-  const theme = useRef("light");
-  const toggleTheme = () => {
-    if (theme.current === "light") {
-      document.querySelector("html").className = "dark";
-      theme.current = "dark";
+  if (!window.localStorage.getItem("theme")) {
+    window.localStorage.setItem("theme", "light")
+  }
+
+
+
+  const [activeTheme, setActiveTheme] = useState(window.localStorage.getItem("theme"))
+  const toggleActiveTheme = () => {
+    activeTheme === "light"
+      ? setActiveTheme(() => "dark")
+      : setActiveTheme(() => "light")
+
+  }
+  const rootElement = useRef(document.querySelector("html"));
+
+
+  useEffect(() => {
+      window.localStorage.setItem("theme", activeTheme)
+  })
+
+  useEffect(() => {
+
+    if (activeTheme === "light") {
+      rootElement.current.className = "light";
     } else {
-      document.querySelector("html").className = "light";
-      theme.current = "light";
+      rootElement.current.className = "dark";
     }
-  };
+
+  },[activeTheme])
+
+
+
+
+
+
+
+
+
+  // const theme = useRef("light");
+  // const toggleTheme = () => {
+  //   if (theme.current === "light") {
+  //     document.querySelector("html").className = "dark";
+  //     theme.current = "dark";
+  //   } else {
+  //     document.querySelector("html").className = "light";
+  //     theme.current = "light";
+  //   }
+  // };
 
   return (
     <div className="App" style={{ height: "100vh", display: "flex", background: "var(--md-sys-color-surface)" }}>
-      <NavigationRail theme={theme.current} toggle={toggleTheme} />
+      <NavigationRail theme={activeTheme} toggle={toggleActiveTheme} />
       <main
         className="Content-container"
         style={{
@@ -38,9 +75,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/reservation' element={<BookingPage />} />
-          <Route path='/successful' element={<SubmissionDialogue heading="Submission Successful" message = "Thank You for reserving your table. Looking forward to see you"/>}/>
+          <Route path='/successful' element={<SubmissionDialogue heading="Submission Successful" message="Thank You for reserving your table. Looking forward to see you" />} />
         </Routes>
-        
+
       </main>
       <footer></footer>
     </div>
