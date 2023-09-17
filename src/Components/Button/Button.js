3d1efe6468228d4cdb33ffea79271567a3826c3c
gulnoor <gulnoor5233@gmail.com>
2023-09-17 19:42:21 +0500

    added navigation hook to reservation button in Hero

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,12 +1,12 @@
 import "./style.css";
-const Button = ({ children,id, type, style={} }) => {
+const Button = ({ children, id, type, style = {}, ...other }) => {
   const styleButton = {
     borderRadius: "100vh",
     minHeight: "48px",
     width: "fit-content",
     padding: "0px 2rem",
     // marginTop:"1rem",
-    fontSize:"1rem",
+    fontSize: "1rem",
     // textOverflow: "ellipsis"
 
 
@@ -23,13 +23,16 @@ const Button = ({ children,id, type, style={} }) => {
     background: "transparent",
     color: "var(--md-sys-color-on-primary-container)",
     borderRadius: "100vh",
-    border:"2px solid var(--md-sys-color-primary)",
+    border: "2px solid var(--md-sys-color-primary)",
     ...style
   };
 
   return (
     <>
-      <button className={id} style={type === "filled" ? filled : outline}>{children}</button>
+      <button className={id}
+        style={type === "filled" ? filled : outline}
+        {...other}
+      >{children}</button>
     </>
   );
 };
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,9 @@
 import "./Hero.css";
 import logo from "../assets/images/Asset 9@4x.png";
 import Button from "./Button/Button";
+import { useNavigate } from "react-router-dom";
 const Hero = () => {
+  const navigate = useNavigate();
   const buttonStyle = {height:"52px",fontSize:"1.2rem"};
   // const heroRef = useRef(null);
   // let heroHeight = "";
@@ -27,7 +29,7 @@ const Hero = () => {
         <h2>Chicago</h2>
         <span>traditional recipes served with a modern twist</span>
         <Button type="outline" style={buttonStyle}>Order Online</Button>
-        <Button id="res" type="filled" style={buttonStyle}>Reserve a Table</Button>
+        <Button id="res" type="filled" style={buttonStyle} onClick={()=>{navigate("/reservation")}}>Reserve a Table</Button>
 
       </div>
       <div
