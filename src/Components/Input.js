cbbd17158e35674b9d9a131fb7d81b8f8721296e
gulnoor <gulnoor5233@gmail.com>
2023-09-19 19:17:27 +0500

    added form validation with "yup" module

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,202 +1,267 @@
-import { useEffect, useState } from "react"
-import "./Input.css"
+import { useEffect, useState } from "react";
+import "./Input.css";
 
-const Input = ({ value, gridArea, children, id, type, styleInput = {}, styleContainer = {}, bookingState, ...other }) => {
-
-    const [isFocused, setIsFocused] = useState(false);
-
-    const [inputStyle, setInputStyle] = useState({
-        zIndex: "42",
-        position: "relative",
-        border: "none",
-        borderRadius: "6px",
-        padding: "4px",
-        fontSize: "1.1rem",
-        background: "transparent",
-        color: type === "date" || type === "time" || type === "select" ? "transparent" : "var(--md-sys-color-on-surface)",
-        outline: "none",
-        width: "100%",
-        ...styleInput
-    })
-    const [containerStyle, setContainerStyle] = useState({
-        position: "relative",
-        display: "flex",
-        alignItems: "center",
-        border: "2px solid var(--md-sys-color-outline)",
-        borderRadius: "6px",
-        height: "56px",
-        padding: "8px",
-        margin: "0 0 0.8rem 0",
-        ...styleContainer
-    })
-
-    const [labelStyle, setLabelStyle] = useState({
-        position: "absolute",
-        padding: "1px 8px",
-        background: "var(--md-sys-color-surface-container)",
-        top: "50%",
-        left: "0.5rem",
-        transform: "translate(0,-50%)",
-        transition: "0.2s",
-        zIndex: "41",
-        borderRadius: "3px",
-        whiteSpace: "nowrap"
-
-    })
-
-    const handleBlur = () => {
-        setIsFocused(false);
-    }
-
-    const handleFocus = () => {
-        setIsFocused(true);
-
-    }
-
-
-    useEffect(() => {
-
-        if (isFocused || value) {
-            setLabelStyle((labelStyle) => {
-                return {
-                    ...labelStyle,
-                    top: "-1px",
-                    margin: "0px 3px",
-                    fontSize: "16px",
-                    color: "var(--md-sys-color-primary)",
-                    zIndex: "43"
-                }
-            })
-            setInputStyle(inputStyle => {
-                return {
-                    ...inputStyle,
-                    color: type === "date" || type === "time" || type === "select" ? "var(--md-sys-color-on-surface)" : inputStyle.color,
-                }
-            })
-
-        }
-        else if (!value) {
-
-            setLabelStyle((labelStyle) => {
-                return {
-                    ...labelStyle,
-                    top: "50%",
-                    fontSize: "1.1rem",
-                    margin: "0px 0px",
-                    color: "var(--md-sys-color-on-surface)",
-                    zIndex: "41"
-                }
-            });
-            setInputStyle(inputStyle => {
-                return {
-                    ...inputStyle,
-                    color: type === "date" || type === "time" || type === "select" ? "transparent" : inputStyle.color,
-                }
-            })
-
-        }
-        if (!isFocused && value) {
-            setLabelStyle((labelStyle) => {
-                return {
-                    ...labelStyle,
-                    color: "var(--md-sys-color-on-surface)",
-
-                }
-            })
-
-        }
-
-    }, [isFocused, value])
-
-    useEffect(() => {
-        isFocused ? setContainerStyle((containerStyle) => {
-            return {
-                ...containerStyle,
-                border: "3px solid var(--md-sys-color-primary)"
-            }
-        })
-            : setContainerStyle((containerStyle) => {
-                return {
-                    ...containerStyle,
-                    border: "2px solid var(--md-sys-color-outline)"
-                }
-            });
-    }, [isFocused])
-
-    switch (type) {
-        case "select":
-            return (
-                <div style={containerStyle} className={`input-container ${children}`}>
-                    <label
-                        htmlFor={id} style={labelStyle}>{children}</label>
-                    <select style={inputStyle} name={id}
-                        id={id}
-                        onChange={(e) => {
-                            bookingState.setBookingData((prev) => {
-                                return {
-                                    ...prev,
-                                    [e.target.id]: e.target.value
-                                }
-                            })
-                        }}
-                        onBlur={handleBlur}
-                        value={value}
-                        onFocus={handleFocus}>
-                        <option value="">Select {`${children}`}</option>
-                        {other.choices.map((choice) => {
-                            return <option key={choice} value={choice}>{choice}</option>
-                        })}
-                    </select>
-                </div>
-            )
-        case "text-area":
-            return (
-                <div style={{ ...containerStyle, height: "auto" }} className={`input-container ${children}`}>
-                    <label
-                        htmlFor={id} style={labelStyle}>{children}</label>
-                    <textarea style={inputStyle} id={id}
-                        onChange={(e) => {
-                            bookingState.setBookingData((prev) => {
-                                return {
-                                    ...prev,
-                                    [e.target.id]: e.target.value
-                                }
-                            })
-                        }}
-                        onBlur={handleBlur}
-                        value={value}
-                        onFocus={handleFocus}
-                        name={id}
-                        cols="30"
-                        rows="10">
-                    </textarea>
-                </div>
-            )
-        default:
-            return (
-
-                <div style={containerStyle} className={`input-container ${children}`}>
-                    <label
-                        htmlFor={id} style={labelStyle}>{children}</label>
-                    <input style={inputStyle}
-                        name={id}
-                        onBlur={handleBlur}
-                        value={value}
-                        onFocus={handleFocus}
-                        id={id}
-                        type={type}
-                        onChange={(e) => {
-                            bookingState.setBookingData((prev) => {
-                                return {
-                                    ...prev,
-                                    [e.target.id]: e.target.value
-                                }
-                            })
-                        }}
-                        {...other} />
-                </div>
-            )
-    }
-
+const Input = ({
+  value,
+  gridArea,
+  children,
+  id,
+  type,
+  styleInput = {},
+  styleContainer = {},
+  bookingState,
+  schema,
+  err,
+  ...other
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+//   const [error, setError] = useState("");
+  const validationScheme = schema.fields[id];
+  const styleDiv = {
+    display:"flex",
+    flexDirection:"column"
 }
+
+  const [inputStyle, setInputStyle] = useState({
+    zIndex: "42",
+    position: "relative",
+    border: "none",
+    borderRadius: "6px",
+    padding: "4px",
+    fontSize: "1.1rem",
+    background: "transparent",
+    color:
+      type === "date" || type === "time" || type === "select"
+        ? "transparent"
+        : "var(--md-sys-color-on-surface)",
+    outline: "none",
+    width: "100%",
+    ...styleInput,
+  });
+  const [containerStyle, setContainerStyle] = useState({
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    border: "2px solid var(--md-sys-color-outline)",
+    borderRadius: "6px",
+    // height: "56px",
+    padding: "8px",
+    margin: "0 0 1rem 0",
+    ...styleContainer,
+  });
+
+  const [labelStyle, setLabelStyle] = useState({
+    position: "absolute",
+    padding: "1px 8px",
+    background: "var(--md-sys-color-surface-container)",
+    top: "50%",
+    left: "0.5rem",
+    transform: "translate(0,-50%)",
+    transition: "0.2s",
+    zIndex: "41",
+    borderRadius: "3px",
+    whiteSpace: "nowrap",
+  });
+  
+  const isValid = () => {
+      console.log("validating "+id)
+    validationScheme
+      .validate(value)
+      .then(() => {
+        err.setError((prev) => {
+            return{...prev,[id]:""}});
+      })
+      .catch((e) => {
+        err.setError((prev) => {
+            return{...prev,[id]:e.message}});
+      });
+  };
+
+  const changeHandler = (e) => {
+    bookingState.setBookingData((prev) => {
+      return {
+        ...prev,
+        [e.target.id]: e.target.value,
+      };
+    });
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    isValid();
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  useEffect(() => {
+    if (isFocused || value) {
+      setLabelStyle((labelStyle) => {
+        return {
+          ...labelStyle,
+          top: "-1px",
+          margin: "0px 3px",
+          fontSize: "16px",
+          color: "var(--md-sys-color-primary)",
+          zIndex: "43",
+        };
+      });
+      setInputStyle((inputStyle) => {
+        return {
+          ...inputStyle,
+          color:
+            type === "date" || type === "time" || type === "select"
+              ? "var(--md-sys-color-on-surface)"
+              : inputStyle.color,
+        };
+      });
+    } else if (!value) {
+      setLabelStyle((labelStyle) => {
+        return {
+          ...labelStyle,
+          top: "50%",
+          fontSize: "1.1rem",
+          margin: "0px 0px",
+          color: "var(--md-sys-color-on-surface)",
+          zIndex: "41",
+        };
+      });
+      setInputStyle((inputStyle) => {
+        return {
+          ...inputStyle,
+          color:
+            type === "date" || type === "time" || type === "select"
+              ? "transparent"
+              : inputStyle.color,
+        };
+      });
+    }
+    if (!isFocused && value) {
+      setLabelStyle((labelStyle) => {
+        return {
+          ...labelStyle,
+          color: "var(--md-sys-color-on-surface)",
+        };
+      });
+    }
+  }, [isFocused, value]);
+
+  useEffect(() => {
+    isFocused
+      ? setContainerStyle((containerStyle) => {
+          return {
+            ...containerStyle,
+            border: "3px solid var(--md-sys-color-primary)",
+          };
+        })
+      : setContainerStyle((containerStyle) => {
+          return {
+            ...containerStyle,
+            border: "2px solid var(--md-sys-color-outline)",
+          };
+        });
+  }, [isFocused]);
+
+  switch (type) {
+    case "select":
+      return (
+        <div style={styleDiv}
+        className={`big-container ${children}`}>
+
+        <div style={containerStyle} className={`input-container ${children}`}>
+          <label htmlFor={id} style={labelStyle}>
+            {children}
+          </label>
+          <select
+            style={inputStyle}
+            name={id}
+            id={id}
+            onChange={changeHandler}
+            onBlur={handleBlur}
+            value={value}
+            onFocus={handleFocus}
+          >
+            <option value="">Select {`${children}`}</option>
+            {other.choices.map((choice) => {
+              return (
+                <option key={choice} value={choice}>
+                  {choice}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+          {err.error[id] ? (
+            <span
+              style={{
+                display: "flex",
+              }}
+              className="form-error"
+            >
+              {err.error[id]}
+            </span>
+          ) : null}
+
+        </div>
+      );
+    case "text-area":
+      return (
+        <div 
+          style={containerStyle}
+          className={`input-container ${children}`}
+        >
+          <label htmlFor={id} style={labelStyle}>
+            {children}
+          </label>
+          <textarea
+            style={inputStyle}
+            id={id}
+            onChange={changeHandler}
+            onBlur={handleBlur}
+            value={value}
+            onFocus={handleFocus}
+            name={id}
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+      );
+    default:
+      return (
+        <div
+        className={`big-container ${children}`}
+        style={styleDiv}
+        >
+          <div style={containerStyle} className={`input-container ${children}`}>
+            <label htmlFor={id} style={labelStyle}>
+              {children}
+            </label>
+            <input
+              style={inputStyle}
+              name={id}
+              onBlur={handleBlur}
+              value={value}
+              onFocus={handleFocus}
+              id={id}
+              type={type}
+              onChange={changeHandler}
+              {...other}
+            />
+          </div>
+          {err.error[id] ? (
+            <span
+              style={{
+                display: "flex",
+              }}
+              className="form-error"
+            >
+              {`* ${err.error[id]}`}
+            </span>
+          ) : null}
+        </div>
+      );
+  }
+};
 export default Input;
--- src/Components/ReservationForm.js
+++ src/Components/ReservationForm.js
@@ -1,15 +1,36 @@
-import React, { useEffect } from 'react'
-import Input from './Input';
-import Button from './Button/Button';
-import serialize from 'form-serialize';
-import { submitAPI } from '../availTimesAPI';
-import { useNavigate } from 'react-router-dom';
-
+import Input from "./Input";
+import Button from "./Button/Button";
+import serialize from "form-serialize";
+import { submitAPI } from "../availTimesAPI";
+import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import { useState } from "react";
 const ReservationForm = ({ bookingState, availTimes }) => {
   const navigate = useNavigate();
 
-
-
+  const validationSchema = yup.object({
+    date: yup.date(),
+    persons: yup.number().min(1, "min 1 person").max(10).required(),
+    time: yup.string().required(),
+    occasion: yup.string().optional(),
+    firstName: yup
+      .string()
+      .required("No name no party")
+      .min(1, "Name must be at least one character long"),
+    lastName: yup.string().optional(),
+    email: yup.string().email(),
+    specialRequest: yup.string().optional(),
+  });
+  const [error, setError] = useState({
+    persons: "",
+    date: "",
+    time: "",
+    occasion: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    specialRequest: "",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,29 +39,108 @@ const ReservationForm = ({ bookingState, availTimes }) => {
     submitAPI(data)
       .then(function () {
         console.log("successful");
-        navigate("/successful")
-      }).catch((e) => console.log(e))
-  }
+        navigate("/successful");
+      })
+      .catch((e) => console.log(e));
+  };
+
   return (
     <form
       style={{
-        display: "flex", flexWrap: "wrap",
+        display: "flex",
+        flexWrap: "wrap",
         maxWidth: "600px",
         justifyContent: "space-between",
         padding: "2rem",
       }}
-      onSubmit={handleSubmit}>
-      <Input bookingState={bookingState} id={"persons"} value={bookingState.bookingData.persons} min={1} max={10} type={"number"}>Persons</Input>
-      <Input bookingState={bookingState} id={"date"} value={bookingState.bookingData.date} type={"date"}>Date</Input>
-      <Input bookingState={bookingState} id={"time"} value={bookingState.bookingData.time} type={"select"} choices={availTimes}>Time</Input>
-      <Input bookingState={bookingState} id={"occasion"} value={bookingState.bookingData.occasion} type={"select"} choices={["Birthday", "Aniversary"]}>Occasion</Input>
-      <Input bookingState={bookingState} id={"firstName"} value={bookingState.bookingData.firstName} >First Name</Input>
-      <Input bookingState={bookingState} id={"lastName"} value={bookingState.bookingData.lastName} >Last Name</Input>
-      <Input bookingState={bookingState} id={"email"} value={bookingState.bookingData.email} type={"email"} >Email</Input>
-      <Input bookingState={bookingState} id={"specialRequest"} value={bookingState.bookingData.specialRequest} type={"text-area"}>Special Request</Input>
-      <Button type={"filled"} >Make your Reservation</Button>
+      onSubmit={handleSubmit}
+    >
+      <Input
+        schema={validationSchema}
+        bookingState={bookingState}
+        id={"persons"}
+        value={bookingState.bookingData.persons}
+        type={"number"}
+        err={{error,setError}}
+      >
+        Persons
+      </Input>
+      <Input
+        schema={validationSchema}
+        bookingState={bookingState}
+        id={"date"}
+        value={bookingState.bookingData.date}
+        type={"date"}
+        err={{error,setError}}
+
+      >
+        Date
+      </Input>
+      <Input
+        schema={validationSchema}
+        bookingState={bookingState}
+        id={"time"}
+        value={bookingState.bookingData.time}
+        type={"select"}
+        choices={availTimes}
+        err={{error,setError}}
+
+      >
+        Time
+      </Input>
+      <Input
+        schema={validationSchema}
+        bookingState={bookingState}
+        id={"occasion"}
+        value={bookingState.bookingData.occasion}
+        type={"select"}
+
+        err={{error,setError}}
+        choices={["Birthday", "Aniversary"]}
+      >
+        Occasion
+      </Input>
+      <Input
+        schema={validationSchema}
+        bookingState={bookingState}
+        id={"firstName"}
+        value={bookingState.bookingData.firstName}
+        err={{error,setError}}
+      >
+        First Name
+      </Input>
+      <Input
+        schema={validationSchema}
+        bookingState={bookingState}
+        id={"lastName"}
+        value={bookingState.bookingData.lastName}
+        err={{error,setError}}
+      >
+        Last Name
+      </Input>
+      <Input
+        schema={validationSchema}
+        bookingState={bookingState}
+        id={"email"}
+        value={bookingState.bookingData.email}
+        type={"email"}
+        err={{error,setError}}
+      >
+        Email
+      </Input>
+      <Input
+        schema={validationSchema}
+        bookingState={bookingState}
+        id={"specialRequest"}
+        value={bookingState.bookingData.specialRequest}
+        type={"text-area"}
+        err={{error,setError}}
+      >
+        Special Request
+      </Input>
+      <Button type={"filled"}>Make your Reservation</Button>
     </form>
-  )
-}
+  );
+};
 
 export default ReservationForm;
